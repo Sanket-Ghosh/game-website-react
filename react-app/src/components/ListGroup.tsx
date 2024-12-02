48fe128dd0e68f859e7b24cf5378b267053d733f
Sanket-Ghosh <sanketghosh003@gmail.com>
2024-12-02 10:34:51 +0530

    Rendering List Items Dynamically in JSX

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+function ListGroup() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "Tokyo",
+    "Delhi",
+    "London",
+    "Paris",
+  ];
+
+  return (
+    <>
+      <h1>Header</h1>
+      <ul className="list-group">
+        {items.map((items) => (
+          <li key={items}>{items}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
